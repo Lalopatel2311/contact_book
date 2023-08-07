20fa92f7563039c9479ebb634830bc8775d261ff
Viraj Asodariya <115859587+virajasodariya@users.noreply.github.com>
2023-08-07 14:32:55 +0530

    try to push

--- lib/View/Auth/register.dart
+++ lib/View/Auth/register.dart
@@ -3,10 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 import 'package:raj_contact_book/Constants/color.dart';
 import 'package:raj_contact_book/Constants/text_style.dart';
 import 'package:raj_contact_book/Controller/variable.dart';
+import 'package:raj_contact_book/View/Auth/log_in.dart';
 import 'package:raj_contact_book/View/Widget/showToast.dart';
 import 'package:raj_contact_book/View/Widget/text_form_field.dart';
 
@@ -22,15 +23,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   DocumentReference user = FirebaseFirestore.instance.collection('User').doc();
   FirebaseAuth auth = FirebaseAuth.instance;
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: PickColor.kBlack,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 75),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: Form(
             key: formKey,
             child: Column(
@@ -43,11 +44,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     style: FontTextStyle.kWhite30W500,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 Text(
-                  "Let us know about your self",
+                  "Let us know about yourself",
                   style: TextStyle(
                     color: Colors.grey.shade600,
                     fontSize: 18,
@@ -57,90 +58,126 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   height: 20.sp,
                 ),
                 CommonTextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    hintText: "Enter your name",
-                    controller: RegisterVariable.nameController,
-                    title: "Name"),
+                  keyboardType: TextInputType.name,
+                  hintText: "Enter your name",
+                  controller: RegisterVariable.nameController,
+                  title: "Name",
+                ),
                 SizedBox(
                   height: 10.sp,
                 ),
                 CommonTextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    hintText: "Enter your email address",
-                    controller: RegisterVariable.emailController,
-                    title: "Email"),
+                  keyboardType: TextInputType.emailAddress,
+                  hintText: "Enter your email address",
+                  controller: RegisterVariable.emailController,
+                  title: "Email",
+                ),
                 SizedBox(
                   height: 10.sp,
                 ),
                 CommonTextFormField(
-                    keyboardType: TextInputType.number,
-                    hintText: "Enter your password",
-                    controller: RegisterVariable.passwordController,
-                    title: "Password"),
+                  keyboardType: TextInputType.number,
+                  hintText: "Enter your password",
+                  controller: RegisterVariable.passwordController,
+                  title: "Password",
+                ),
                 SizedBox(
                   height: 10.sp,
                 ),
                 CommonTextFormField(
-                    keyboardType: TextInputType.number,
-                    hintText: "Confirm your password",
-                    controller: RegisterVariable.repeatPasswordController,
-                    title: "Confirm Password"),
+                  keyboardType: TextInputType.number,
+                  hintText: "Re-enter your password",
+                  controller: RegisterVariable.repeatPasswordController,
+                  title: "Reenter Password",
+                ),
                 const SizedBox(
                   height: 20,
                 ),
                 const SizedBox(
                   height: 30,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: width * 0.1,
-                  ),
-                  child: OutlinedButton(
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        if (RegisterVariable.passwordController.text ==
-                            RegisterVariable.repeatPasswordController.text) {
-                          try {
-                            UserCredential userCredential =
-                                await auth.createUserWithEmailAndPassword(
-                              email: RegisterVariable.emailController.text,
-                              password:
-                                  RegisterVariable.passwordController.text,
-                            );
+                Visibility(
+                  visible: !isLoading,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 100.w,
+                    ),
+                    child: OutlinedButton(
+                      onPressed: () async {
+                        setState(() {
+                          isLoading =
+                              true; // Show the loading indicator when the button is pressed.
+                        });
+                        if (formKey.currentState!.validate()) {
+                          if (RegisterVariable.passwordController.text ==
+                              RegisterVariable.repeatPasswordController.text) {
+                            try {
+                              UserCredential userCredential =
+                                  await auth.createUserWithEmailAndPassword(
+                                email: RegisterVariable.emailController.text,
+                                password:
+                                    RegisterVariable.passwordController.text,
+                              );
 
-                            user
-                                .collection('${userCredential.user!.email}')
-                                .add({
-                              "name": RegisterVariable.nameController.text,
-                              "email": RegisterVariable.emailController.text,
-                              "password":
-                                  RegisterVariable.passwordController.text,
-                              "re password":
-                                  RegisterVariable.passwordController.text,
+                              // Save data to Firebase
+                              user
+                                  .collection('${userCredential.user!.email}')
+                                  .add({
+                                "name": RegisterVariable.nameController.text,
+                                "email": RegisterVariable.emailController.text,
+                                "password":
+                                    RegisterVariable.passwordController.text,
+                                "re password":
+                                    RegisterVariable.passwordController.text,
+                              });
+
+                              Get.offAll(const LogInScreen());
+                            } on FirebaseAuthException catch (error) {
+                              log('ERROR---------->>>>>>>>>>${error.code}');
+                              showToast("${error.message}");
+                            } finally {
+                              setState(() {
+                                isLoading =
+                                    false; // Hide the loading indicator when the task is done.
+                              });
+                            }
+                          } else {
+                            showToast("Password doesn't match");
+                            setState(() {
+                              isLoading =
+                                  false; // Hide the loading indicator when the task is done.
                             });
-                          } on FirebaseAuthException catch (error) {
-                            log('ERROR---------->>>>>>>>>>${error.code}');
-
-                            showToast("${error.message}");
                           }
                         } else {
-                          showToast("password doesn't match");
+                          setState(() {
+                            isLoading =
+                                false; // Hide the loading indicator when the task is done.
+                          });
                         }
-                      }
-                    },
-                    style: OutlinedButton.styleFrom(
-                      fixedSize: const Size(360, 45),
-                      side: const BorderSide(
-                        color: PickColor.k7B7B7B,
-                        width: 1.50,
+                      },
+                      style: OutlinedButton.styleFrom(
+                        fixedSize: const Size(360, 45),
+                        side: const BorderSide(
+                          color: PickColor.k7B7B7B,
+                          width: 1.50,
+                        ),
+                      ),
+                      child: const Text(
+                        "Next",
+                        style: TextStyle(
+                          color: PickColor.kWhite,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                    child: const Text(
-                      "Next",
-                      style: TextStyle(
-                        color: PickColor.kWhite,
-                        fontSize: 18,
-                      ),
+                  ),
+                ),
+                Visibility(
+                  visible: isLoading,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Center(
+                      child: CircularProgressIndicator(),
                     ),
                   ),
                 ),
