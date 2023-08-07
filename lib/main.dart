6eabce01604e24771f0c4a1b07f948c9b89b21c4
SahilKumar Sorathiya <sorathiyasahil5656@gmail.com>
2023-08-07 14:58:28 +0530

    mane nathu khabar

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:raj_contact_book/View/Auth/log_in.dart';
 import 'package:raj_contact_book/View/Auth/register.dart';
-import 'package:raj_contact_book/View/Auth/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +13,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  ///i sahil
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
